2668a710d4526f1bfa0af457d3d1bf6c867d0f16
Armin Namavari <arminn@stanford.edu>
2018-06-03 17:42:42 -0700

    implemented indexing

--- rb_tree.py
+++ rb_tree.py
@@ -1,67 +1,63 @@
 '''
 Provides an implementation of a radix-balanced tree in python. This data
 structure supports immutable updates.
+How to properly support delete/split?
+Implementation based heavily on: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.1000.6710&rep=rep1&type=pdf
 '''
+B = 1
+NODE_SIZE = 1 << B
 
-class RBNode:
-    def __init__(B):
-        '''
-        M = 1 << Bis the branching factor
-        '''
-        self.B = B
-        self.M = 1 << B
-        self.slots = [None for i in range(M)]
 
-    def lookup(idx, shift):
+class RBNode: # TODO: should each node maintain a depth?
+    def __init__(self, slots):
+        self.slots = slots
+        self.next_free_slot = 0 # index of the next free slot
+
+    def get(self, index, depth):
         '''
         Return what value is stored at index idx stored at the subtree rooted
         at this node
         '''
-        if shift: # are we at a leaf?
-            return self.slots[idx]
+        if depth == 1: # are we at a leaf?
+            return self.slots[index]
         else:
-            slot_idx = idx >> shift # read only the first B bits of the number
-            return self.slots[slot_idx].lookup(idx << B, shift << 1)
+            slot_idx = index >> (B * (depth - 1)) # read only the first B bits of the number
+            next_idx = ((1 << (B * (depth - 1))) - 1) & index
+            return self.slots[slot_idx].get(next_idx, depth - 1)
+
 
 class RBTree:
-    def __init__(B):
+    def __init__(self):
         '''
         M = 1 << B is the branching factor
         '''
         self.size = 0
-        self.B = B
-        self.root = RBNode(1 << B)
-        self.shift = 0 # B * height
-        self.start_idx = 0
-        self.end_idx = 0
+        self.root = RBNode([None for i in range(NODE_SIZE)])
+        self.depth = 1
 
-    def append(elem):
-        '''
-        Append elem to the data structure
-        Cases to handle: space exists in the right most leaf, might have to
-        create a new node, might also have to create a new root.
-        '''
-        pass
+    def get(self, index):
+        return self.root.get(index, self.depth)
+    
 
-    def lookup(idx):
-        '''
-        Returns what value is stored at index idx
-        '''
-        return root.lookup(idx, shift)
-
-    def update(idx, val):
-        '''
-        Returns a new RBTree -- need to copy the path from the root to the
-        desired node
-        '''
-        pass
+def index_test():
+    # assumes B = 1
+    leaf0, leaf1 = RBNode([0, 1]), RBNode([2, 3])
+    leaf2, leaf3 = RBNode([4, 5]), RBNode([6, 7])
+    internal0, internal1 = RBNode([leaf0, leaf1]), RBNode([leaf2, leaf3])
+    root = RBNode([internal0, internal1])
+    tree = RBTree()
+    tree.depth = 3
+    tree.root = root
+    for i in range(8):
+        assert(i == tree.get(i))
+    print('index_test passed!')
 
 def test():
     '''
     Tests the structure
     TODO: also write a nice display function for the structure
     '''
-    pass
+    index_test()
 
 if __name__=='__main__':
     test()
